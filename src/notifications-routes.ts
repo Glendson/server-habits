b33b6e817f4f160b79c32ae528f1c5cc8bd9a2e9
Glendson <gztomazetto@gmail.com>
2023-02-08 21:42:40 +0000

    routes notification implemented

--- src/notifications-routes.ts
+++ src/notifications-routes.ts
@@ -1,5 +1,6 @@
 import { FastifyInstance } from "fastify"
 import WebPush from "web-push"
+import { z } from "zod"
 
 
 const publicKey = 'BIhWlKVcbZfjlswXpttFK8UUc86IRNgl7h4feEQPpzDiBCn0kYRLmEdPKrNQJkc-WbedTOVe2-SzJcF6tfxk2kk'
@@ -23,6 +24,18 @@ export async function notificationRoutes(app: FastifyInstance) {
     })
 
     app.post("/push/send", async (req, res) => {
+        const sendPushBody = z.object({
+            subscription: z.object({
+                endpoint: z.string(),
+                keys: z.object({
+                    p256dh: z.string(),
+                    auth: z.string(),
+                })
+            })
+        })
 
+        const { subscription } = sendPushBody.parse(req.body)
+
+        WebPush.sendNotification(subscription, "BackEnd Hello!")
     })
 }
